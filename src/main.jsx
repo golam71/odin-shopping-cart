5312d3df0e6005ca55b9cdd13aa7ba10ce6664d9
Golam <74001687+golam71@users.noreply.github.com>
2026-06-19 11:24:38 +0600

    ✨ Create the browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,28 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+
 import App from "./App.jsx";
+import Home from "./home/Home.jsx";
+import Products from "./prodcuts/Products.jsx";
+import Cart from "./cart/Cart.jsx";
 
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
+    Component: App,
+    children: [
+      { index: true, Component: Home },
+      { path: "/products", Component: Products },
+      { path: "/cart", Component: Cart },
+    ],
   },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />,
+    <RouterProvider router={router} />
   </StrictMode>,
 );
